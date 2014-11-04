50460de4ed93226019a9fc1cf2ffde51258055fe
Etskh <etskh@hotmail.com>
2014-11-04 11:52:38 -0800

    Fixed URLS

--- gfx/urls.py
+++ gfx/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, url
 from gfx import views
 
 urlpatterns = patterns('',
-	url(r'^mesh/([0-9]+)$', views.get_mesh, name='get_mesh')
-	url(r'^texture/([0-9]+)$', views.get_texture, name='get_texture')
-	url(r'^material/([0-9]+)$', views.get_material, name='get_material')
+	url(r'^mesh/([0-9]+)$', views.get_mesh, name='get_mesh'),
+	url(r'^texture/([0-9]+)$', views.get_texture, name='get_texture'),
+	url(r'^material/([0-9]+)$', views.get_material, name='get_material'),
+	url(r'^shader/([0-9]+)$', views.get_shader, name='get_shader'),
 )
 
 
