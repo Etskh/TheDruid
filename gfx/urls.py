0e080d2cba1a1d39a9951c8147e4a9430cb3b783
Etskh <etskh@hotmail.com>
2014-11-03 16:08:44 -0800

    Added in urls and views for the gfx module

--- gfx/urls.py
+++ gfx/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, url
 from gfx import views
 
 urlpatterns = patterns('',
-	url(r'^mesh/(?P<mesh_id>\d+)$', views.get_mesh, name='get_mesh')
+	url(r'^mesh/([0-9]+)$', views.get_mesh, name='get_mesh')
+	url(r'^texture/([0-9]+)$', views.get_texture, name='get_texture')
+	url(r'^material/([0-9]+)$', views.get_material, name='get_material')
 )
 
 
--- gfx/views.py
+++ gfx/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-def get_mesh( request, poll_id):
-	return HttpResponse("You're looking at mesh %s." % mesh_id)
 
+def get_mesh( request, mesh_id):
+	return HttpResponse("You're looking at mesh %s." % mesh_id )
+
+def get_texture( request, texture_id):
+	return HttpResponse("You're looking at texture %s." % texture_id )
+
+def get_material( request, material_id):
+	return HttpResponse("You're looking at material %s." % material_id )
 
